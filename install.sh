d1279b286ff9864aa9a58224aa4e20463a117072
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-24 00:22:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 wget -O repos.sh https://github.com/SteavenBuntu/SteavenRepos/raw/main/install.sh
 wget -O gnome.sh https://github.com/SteavenBuntu/SteavenGnome/raw/main/install.sh
+wget -O wallpapers.sh https://github.com/SteavenBuntu/SteavenBuntu-Wallpapers/raw/main/install.sh
+
 sudo chmod +x gnome.sh
 sudo chmod +x repos.sh
+sudo chmod +x wallpapers.sh
 ./repos.sh
 ./gnome.sh
+./wallpapers.sh
