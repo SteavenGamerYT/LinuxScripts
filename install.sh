2238bfc2a8a3cd35081afefbc2810939a4658a99
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-23 05:27:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget -O repos.sh https://github.com/SteavenBuntu/SteavenRepos/raw/main/install.sh
 wget -O gnome.sh https://github.com/SteavenBuntu/SteavenGnome/raw/main/install.sh
-chmod +x gnome.sh
-chmod +x repos.sh
+sudo chmod +x gnome.sh
+sudo chmod +x repos.sh
 ./repos.sh
 ./gnome.sh
