33a8c31ab3a09e0819b4b97187beaf51b9f15f65
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-08-22 23:25:49 +0300

    prime

--- prime-run-nvidia.sh
+++ prime-run-nvidia.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo '#!/bin/bash' | sudo tee -a /usr/bin/prime-run
-echo 'export __NV_PRIME_RENDER_OFFLOAD=1' | sudo tee -a /usr/bin/prime-run
-echo 'export __GLX_VENDOR_LIBRARY_NAME=nvidia' | sudo tee -a /usr/bin/prime-run
-echo 'export __VK_LAYER_NV_optimus=NVIDIA_only' | sudo tee -a /usr/bin/prime-run
-echo 'export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json' | sudo tee -a /usr/bin/prime-run
-echo 'exec "$@"' | sudo tee -a /usr/bin/prime-run
+if grep -q '#!/bin/bash
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+exec "$@"' /usr/bin/prime-run
+    then
+            echo "prime-run IS ALRDAY installed"
+    else
+            echo '#!/bin/bash
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+exec "$@"' | sudo tee -a /usr/bin/prime-run
+ fi
