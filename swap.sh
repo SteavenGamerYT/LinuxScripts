91b3ecd57d024c05283a01e364d83b2f841e71bf
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-08-23 05:42:34 +0300

    swap

--- swap.sh
+++ swap.sh
@@ -13,3 +13,8 @@ if grep -q '# Swap
             echo '# Swap
 /swapfile        none        swap        defaults      0 0' | sudo tee -a /etc/fstab
  fi
+find /boot/loader/entries -type f ! -name "*fallback*" -exec grep -q 'options' {} \; -exec  sudo sed -i "s/options.*/& resume_offset=$(sudo btrfs inspect-internal map-swapfile -r \/swapfile)/" {} +
+diskuuid=$(eval $(lsblk -oMOUNTPOINT,UUID  -P -M | grep 'MOUNTPOINT="/"'); echo $UUID )
+find /boot/loader/entries -type f ! -name "*fallback*" -exec grep -q 'options' {} \; -exec  sudo sed -i "s/options.*/& resume=UUID=$(echo $diskuuid)/" {} +
+sudo sed -i 's/\(HOOKS=.*filesystems\) \(fsck.*\)/\1 resume \2/' /home/omarhanykasban/mkinitcpio.conf
+sudo mkinitcpio -P
