51959085651e26cc77f9c10596b7202bc940da50
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-08-25 16:08:44 +0300

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -Syu --noconfirm --needed flatpak curl
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install com.spotify.Client -y
+flatpak install com.spotify.Client -y
 sudo flatpak override --user --filesystem=~/Music com.spotify.Client
 sudo flatpak update --commit=44e179795004a87090214404d13e672b1873b523f1b6e4863f32390bf34de7c8 com.spotify.Client -y
 sudo flatpak mask com.spotify.Client
