cfdc56eca565356191422d3b7060fe33fd474f7b
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-08-28 16:19:24 +0300

    typo

--- swap.sh
+++ swap.sh
@@ -19,5 +19,5 @@ echo "Enabling hibernation for BTRFS + Systemd-Boot"
 find /boot/loader/entries -type f ! -name "*fallback*" -exec grep -q 'options' {} \; -exec  sudo sed -i "s/options.*/& resume_offset=$(sudo btrfs inspect-internal map-swapfile -r \/swapfile)/" {} +
 diskuuid=$(eval $(lsblk -oMOUNTPOINT,UUID  -P -M | grep 'MOUNTPOINT="/"'); echo $UUID )
 find /boot/loader/entries -type f ! -name "*fallback*" -exec grep -q 'options' {} \; -exec  sudo sed -i "s/options.*/& resume=UUID=$(echo $diskuuid)/" {} +
-sudo sed -i 's/\(HOOKS=.*filesystems\) \(fsck.*\)/\1 resume \2/' /home/omarhanykasban/mkinitcpio.conf
+sudo sed -i 's/\(HOOKS=.*filesystems\) \(fsck.*\)/\1 resume \2/' /etc/mkinitcpio.conf
 sudo mkinitcpio -P
